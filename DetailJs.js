fabaa07fa56729de3166ef2825692c2dd0d7032a
hongjungwook1 <wjddnr0208@naver.com>
2024-12-13 20:27:45 +0900

    hongjungwook1

--- DetailJs.js
+++ DetailJs.js
@@ -1,96 +1,78 @@
-function show_text(event) {
-    //Li 생성
-    const createLi = document.createElement('li');
-
-    //set text value
-    const setInput = document.getElementsByClassName('todo-input')[0].value;
-    
-    // Check Box 생성
-    const checkBox = document.createElement("input");
-    checkBox.type ="checkbox";
-    checkBox.classList.add('todoCheck');
-
-    // Edit 버튼 생성
-    const createEdit = document.createElement("button");
-    createEdit.innerHTML = 'Edit';
-    createEdit.classList.add("editBtn");
-
-    // Save 버튼 생성
-    const createSave = document.createElement("button");
-    createSave.innerHTML = 'Save';
-    createSave.classList.add("saveBtn");
-    createSave.style.display = "none";
-
-    // X 버튼 생성
-    const createXbtn = document.createElement("button");
-    createXbtn.innerHTML = 'X';
-    createXbtn.type = "button";
-
-    // 기본 Span 생성
-    const createSpan = document.createElement("span");
-    createSpan.append(setInput);
-
-    // 수정 시 사용되는 Input text 생성
-    const editInput = document.createElement('input');
-    editInput.type = 'text';
-    editInput.id = 'editInput';
-
-    // 수정 시 사용되는 Span 생성
-    const editSpan = document.createElement("span");
-    editSpan.append(editInput);
-
-    if (!setInput.trim()) {
-        alert("공백 추가 x");
-        return false;
-    }
-
+function insertTodo(event) {
     if(event.key === 'Enter') {
-        createLi.append(checkBox)
-        createLi.append(createSpan)
-        document
-            .getElementById('todo-list')
-            .appendChild(createLi)
-            .append(createEdit , createSave , createXbtn)
-        }
+        createdValue();
+    }
+}
 
-    createXbtn.addEventListener('click', function() {
-        createLi.remove();
-    });
+function createdButton (inner , className) {
+    const createbutton = document.createElement("button");
+    createbutton.innerHTML = inner;
+    createbutton.className = className;
     
-    createEdit.addEventListener('click' , function() {
-        createEdit.style.display = "none";
-        createSave.style.display = "inline";
-        
-        createLi.replaceChild(editSpan , createSpan);
+    return createbutton;
+}
 
-    });
+function createdInput (inner , type , className) {
+    const createdInput = document.createElement("input");
+    createdInput.inner = inner;
+    createdInput.type = type;
+    createdInput.className = className;
+
+    return createdInput;
+}
+
+function createdElement (element , inner , className) {
+    const createdElement = document.createElement(element);
+    createdElement.inner = inner;
+    createdElement.className = className;
     
-    createSave.addEventListener('click' , function() {
-        const saveInput = document.getElementById('editInput').value;
-        
-        if (!saveInput.trim()) {
-            alert("수정 시 공백 x");
-            return false;
-        }
+    return createdElement;
+}
 
-        createSpan.innerText = saveInput;
-        createLi.replaceChild(createSpan , editSpan);
-        
-        createSave.style.display = "none";
-        createEdit.style.display = "inline";
-    });
+function findTopNode (event) {
+    const topNode = event.target.parentElement;
+    return topNode;
+}
 
+function createdValue() {
+    const inputValue = document.querySelector('.todo-input').value.trim();
+    if (inputValue == '') return;
 
+    const newLi = createdElement("li" , "newLi" , "newLi");
+
+    const createdCheckBox = createdInput("checkBox" , "checkBox" , "todoCheckBoxList");
+    newLi.append(createdCheckBox)
+
+    const newSpan = createdElement("span" , "newSpan" , "newSpan");
+    newSpan.append(inputValue);
+    newLi.append(newSpan)
+
+    const createdEdit = createdButton("Edit" , "editBtn");
+    createdEdit.addEventListener('click' , (event) => {clickEditBtn(event)});
+
+    const createdXBtn = createdButton("X" , "xBtn");
+    createdXBtn.addEventListener('click', (event) => {clickXBtn(event);});
+
+    const createdSave = createdButton("Save" , "saveBtn");
+    createdSave.style.display = "none";
+    createdSave.addEventListener('click', (event) => {clickSaveBtn(event);});
+
+    newLi.append(createdEdit , createdSave , createdXBtn);
+
+    const todoList = document.getElementById('todo-list')
+    todoList.append(newLi);
+
+    return todoList;
 }
 
 function todoFilter() {
-    const checkboxList = document.getElementsByClassName('todoCheck');
+    const checkboxList = document.getElementsByClassName('todoCheckBoxList');
     const todoList = document.querySelectorAll('#todo-list li');
     const checkValues = document.getElementById("todo-filter").value;
 
     for(let i = 0; i < todoList.length; i++) {
         const tdcheck = checkboxList[i];
-
+        
         if (checkValues == "all") {
             todoList[i].style.display = "list-item";
         }
@@ -103,7 +85,50 @@ function todoFilter() {
         else {
             todoList[i].style.display = "none";
         }
-
     }
+}
+
+function clickXBtn(event) {
+    const topNode = findTopNode(event);
+    topNode.remove();
+};
+
+function clickEditBtn(event) {
+    const topNode = findTopNode(event);
+
+    const editInput = createdInput("editInput" , "text" , "editInput");
+    const editSpan = createdElement("span" , "editSpan" , "editSpan");
+    editSpan.append(editInput);
+
+    const findSaveBtn = topNode.querySelector(".saveBtn");
+    findSaveBtn.style.display = "inline";
+    
+    const findEditBtn = topNode.querySelector(".editBtn");
+    findEditBtn.style.display = "none";
+    
+    const findXBtn = topNode.querySelector(".xBtn");
+    findXBtn.style.display = "none";
+    
+    const changeSpan = topNode.querySelector("span");
+    topNode.replaceChild(editSpan , changeSpan);
+
+}
+
+function clickSaveBtn (event) {
+    const topNode = findTopNode(event);
+
+    const saveEdit = topNode.querySelector(".editSpan");
+    const saveInput = saveEdit.querySelector(".editInput");
+    
+    saveEdit.innerText = saveInput.value;
+
+    const findSaveBtn = topNode.querySelector(".saveBtn");
+    findSaveBtn.style.display = "none";
+    
+    const findEditBtn = topNode.querySelector(".editBtn");
+    findEditBtn.style.display = "inline";
+    
+    const findXBtn = topNode.querySelector(".xBtn");
+    findXBtn.style.display = "inline";
 
 }
