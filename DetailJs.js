75b0b776ebf4e3b440f7cdd67bfea8f4700fe776
hongjungwook1 <wjddnr0208@naver.com>
2024-12-20 15:34:14 +0900

    Local Stroage 구현

--- DetailJs.js
+++ DetailJs.js
@@ -1,5 +1,15 @@
+window.addEventListener("load" , function() {
+    const getLocalStorageData = getLocalStorage();
+    if (getLocalStorageData) {
+        for (let i = 0; i < getLocalStorageData.length; i++) {
+            showLocalStorage(getLocalStorageData[i]);
+        }
+    }
+});
+
 function insertTodo(event) {
     if(event.key === 'Enter') {
+        setLocalStorage();
         createdValue();
     }
 }
@@ -36,51 +46,59 @@ function findTopNode (event) {
 }
 
 function createdValue() {
-    const inputValue = document.querySelector('.todo-input').value.trim();
-    if (inputValue == '') return;
+    const input = document.querySelector('.todo-input');
+    const inputValue = input.value.trim();
+
+    if (inputValue === '') return;
 
     const newLi = createdElement("li" , "newLi" , "newLi");
-
+    
     const createdCheckBox = createdInput("checkBox" , "checkBox" , "todoCheckBoxList");
     newLi.append(createdCheckBox)
-
+    
     const newSpan = createdElement("span" , "newSpan" , "newSpan");
     newSpan.append(inputValue);
     newLi.append(newSpan)
-
+    
     const createdEdit = createdButton("Edit" , "editBtn");
     createdEdit.addEventListener('click' , (event) => {clickEditBtn(event)});
-
+    
     const createdXBtn = createdButton("X" , "xBtn");
     createdXBtn.addEventListener('click', (event) => {clickXBtn(event);});
-
+    
     const createdSave = createdButton("Save" , "saveBtn");
     createdSave.style.display = "none";
     createdSave.addEventListener('click', (event) => {clickSaveBtn(event);});
-
+    
     newLi.append(createdEdit , createdSave , createdXBtn);
-
+    
     const todoList = document.getElementById('todo-list')
     todoList.append(newLi);
-
+    
+    input.value = '';
+    
     return todoList;
 }
 
-function todoFilter() {
-    const checkboxList = document.getElementsByClassName('todoCheckBoxList');
-    const todoList = document.querySelectorAll('#todo-list li');
+function changeFilter () {
     const checkValues = document.getElementById("todo-filter").value;
+    todoFilter(checkValues);
+}
+
+function todoFilter(checkValues) {
+    const todoList = document.querySelectorAll('#todo-list li');
+    const checkboxList = document.getElementsByClassName('todoCheckBoxList');
 
     for(let i = 0; i < todoList.length; i++) {
         const tdcheck = checkboxList[i];
         
-        if (checkValues == "all") {
+        if (checkValues === "all") {
             todoList[i].style.display = "list-item";
         }
-        else if (checkValues == "todo" && !tdcheck.checked) {
+        else if (checkValues === "todo" && !tdcheck.checked) {
             todoList[i].style.display = "list-item";
             }
-        else if (checkValues == "done" && tdcheck.checked) {
+        else if (checkValues === "done" && tdcheck.checked) {
             todoList[i].style.display = "list-item";
         }
         else {
@@ -91,8 +109,15 @@ function todoFilter() {
 
 function clickXBtn(event) {
     const topNode = findTopNode(event);
+    const getNumber = parseInt(topNode.querySelector("span").id, 10);
+    const getLocalStorageData = getLocalStorage();
+
+    const updateLocalStorageData = getLocalStorageData.filter(item => item.Number !== getNumber);
+    saveLocalStorage(updateLocalStorageData);
+
     topNode.remove();
-};
+
+}
 
 function clickEditBtn(event) {
     const topNode = findTopNode(event);
@@ -117,12 +142,10 @@ function clickEditBtn(event) {
 
 function clickSaveBtn (event) {
     const topNode = findTopNode(event);
-
     const saveEdit = topNode.querySelector(".editSpan");
     const saveInput = saveEdit.querySelector(".editInput");
-    
     saveEdit.innerText = saveInput.value;
-
+    
     const findSaveBtn = topNode.querySelector(".saveBtn");
     findSaveBtn.style.display = "none";
     
@@ -132,4 +155,60 @@ function clickSaveBtn (event) {
     const findXBtn = topNode.querySelector(".xBtn");
     findXBtn.style.display = "inline";
 
+    setLocalStorage();
+
+}
+
+function setLocalStorage () {
+    const saveDataArray = []
+    const todoList = document.querySelector('.todo-list');
+
+    for (let i = 0; i < todoList.children.length; i++) {
+        const getSpan = todoList.children[i].querySelector('span');
+
+        const saveDataObj = {
+            Text : getSpan.innerHTML,
+            Number : parseInt(i , 10)
+        };
+        getSpan.id = i;
+        saveDataArray.push(saveDataObj);
+    }
+    saveLocalStorage(saveDataArray);
+}
+
+function saveLocalStorage(data) {
+    localStorage.setItem("Save" , JSON.stringify(data));
+}
+
+function showLocalStorage (getLocalStorageData) {
+    const newLi = createdElement("li" , "newLi" , "newLi");
+
+    const createdCheckBox = createdInput("checkBox" , "checkBox" , "todoCheckBoxList");
+    newLi.append(createdCheckBox)
+
+    const newSpan = createdElement("span" , "newSpan" , "newSpan");
+    newSpan.append(getLocalStorageData.Text);
+    newSpan.id = parseInt(getLocalStorageData.Number);
+    newLi.append(newSpan);
+
+    const createdEdit = createdButton("Edit" , "editBtn");
+    createdEdit.addEventListener('click' , (event) => {clickEditBtn(event)});
+    
+    const createdXBtn = createdButton("X" , "xBtn");
+    createdXBtn.addEventListener('click', (event) => {clickXBtn(event);});
+    
+    const createdSave = createdButton("Save" , "saveBtn");
+    createdSave.style.display = "none";
+    createdSave.addEventListener('click', (event) => {clickSaveBtn(event);});
+    
+    newLi.append(createdEdit , createdSave , createdXBtn);
+
+    const todoList = document.getElementById('todo-list')
+    todoList.append(newLi);
+    
+}
+
+function getLocalStorage () {
+    const getLocalStorageData = JSON.parse(localStorage.getItem('Save'));
+    return getLocalStorageData;
 }
